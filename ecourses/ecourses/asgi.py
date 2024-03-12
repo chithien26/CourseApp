56e40454b4302239fae9ee635db6b4ded7c0417b
Your Name <lechithien26@gmail.com>
2024-03-12 13:55:32 +0700

    'begin'

--- ecourses/ecourses/asgi.py
+++ ecourses/ecourses/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for course_app project.
+ASGI config for ecourses project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'course_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecourses.settings')
 
 application = get_asgi_application()
